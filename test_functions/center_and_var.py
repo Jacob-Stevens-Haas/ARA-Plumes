e734d439c845d0ed9fc0adb862d706531f15220d
Ike <malachite.wind@gmail.com>
2023-08-09 19:52:33 -0500

    added mouse_callback to choose plume origin.

--- test_functions/center_and_var.py
+++ test_functions/center_and_var.py
@@ -5,10 +5,43 @@ import matplotlib.pyplot as plt
 
 img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0287.png"
 img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0102.png"
-# img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0690.png"
+img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0690.png"
+img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0628.png"
+
+#########################
+## Ask user for center ##
+#########################
+
+# Global variable to store the clicked point
+clicked_point = None
+
+def mouse_callback(event, x, y, flags, param):
+    global clicked_point
+    if event == cv2.EVENT_LBUTTONDOWN:
+        clicked_point = (x, y)
+        print(f"Clicked at ({x}, {y})")
+        cv2.destroyAllWindows()
 
 # Load image 
-image = cv2.imread(img_path,0) # Load in as grayscale
+image = cv2.imread(img_path) 
+
+# Convert to gray
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Create a window and set the mouse callback
+cv2.namedWindow("Image")
+cv2.setMouseCallback("Image", mouse_callback)
+
+# Display the image
+cv2.imshow("Image", image_gray)
+
+# Keep the script running until a click event occurs
+while clicked_point is None:
+    cv2.waitKey(1)  # Check for a key press every 1 millisecond
+
+
+# After a click event, continue with the rest of the script
+print("Clicked point:", clicked_point)
 
 ########################
 ## Apply Thresholding ##
@@ -19,7 +52,7 @@ image = cv2.imread(img_path,0) # Load in as grayscale
 # _, threshold = cv2.threshold(image, threshold_value, 255, cv2.THRESH_BINARY)
 
 # OTSU thresholding (automatically choose params)
-_, threshold = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+_, threshold = cv2.threshold(image_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 # Adaptive Thresholding
 # threshold = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, constant)
@@ -29,7 +62,7 @@ _, threshold = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 ############################
 
 # Find Contours
-contours, hierarchy = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # print("hierarchy type:", type(hierarchy))
 # print("hierarchy:", hierarchy)
@@ -38,7 +71,7 @@ contours, hierarchy = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_A
 # print("contours shape:", len(contours))
 
 # Select n largest contours
-n=2
+n=1
 contours = sorted(contours,key=cv2.contourArea, reverse = True)
 
 for c in contours[:n]:
@@ -55,34 +88,35 @@ cv2.imshow("Image with Contours", contour_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
 ###################################
 ## Get center points of contours ##
 ###################################
 
-# Calculate center of mass
-center_points = []
+# # Calculate center of mass
+# center_points = []
 
-for contour in selected_contours:
-    # Calculate moments of the contour
-    moments = cv2.moments(contour)
+# for contour in selected_contours:
+#     # Calculate moments of the contour
+#     moments = cv2.moments(contour)
 
-    # Calculate center of mass
-    if moments["m00"] !=0:
-        center_x = int(moments["m10"] / moments["m00"])
-        center_y = int(moments["m01"] / moments["m00"])
-        center_points.append((center_x, center_y))
+#     # Calculate center of mass
+#     if moments["m00"] !=0:
+#         center_x = int(moments["m10"] / moments["m00"])
+#         center_y = int(moments["m01"] / moments["m00"])
+#         center_points.append((center_x, center_y))
 
-##############################
-## Plot centers of contours ##
-##############################
+# ##############################
+# ## Plot centers of contours ##
+# ##############################
 
-# Convert the OpenCV image to a format compatible with matplotlib
-plt_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# # Convert the OpenCV image to a format compatible with matplotlib
+# plt_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# Create a scatter plot for the center points
-plt.figure(figsize=(8, 6))
-plt.imshow(plt_image)
-plt.scatter(*zip(*center_points), color='red', marker='x', s=50)
-plt.title("Plume with Center Points")
-plt.axis('off')
-plt.show()
+# # Create a scatter plot for the center points
+# plt.figure(figsize=(8, 6))
+# plt.imshow(plt_image)
+# plt.scatter(*zip(*center_points), color='red', marker='x', s=50)
+# plt.title("Plume with Center Points")
+# plt.axis('off')
+# plt.show()
