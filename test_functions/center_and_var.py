3e31de4973d5411e62da4dcb5997e2f77265148c
Ike <malachite.wind@gmail.com>
2023-08-09 23:12:59 -0500

    added concentric circles

--- test_functions/center_and_var.py
+++ test_functions/center_and_var.py
@@ -3,14 +3,15 @@
 import cv2
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
 sys.path.append('/Users/Malachite/Documents/UW/ARA/ARA-Plumes/')
 
-from utils import ImagePointPicker
+from utils import ImagePointPicker, find_max_on_boundary, find_next_center
 
-# img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0287.png"
+img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0287.png"
 # img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0102.png"
 # img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0690.png"
-img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0628.png"
+# img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/video_low_1/fixed_avg_frames/subtract_0628.png"
 
 #########################
 ## Ask user for center ##
@@ -18,10 +19,8 @@ img_path = "/Users/Malachite/Documents/UW/ARA/ARA-Plumes/plume_videos/July_20/vi
 
 image_to_click = ImagePointPicker(img_path)
 image_to_click.ask_user()
-center = image_to_click.clicked_point 
+orig_center = image_to_click.clicked_point 
 
-print("test")
-print(center)
 
 # Load image and convert to gray
 image = cv2.imread(img_path)
@@ -58,7 +57,98 @@ selected_contours = contours[:n]
 # Draw contours on the original image (or a copy of it)
 contour_image_original = image.copy()
 cv2.drawContours(contour_image_original, selected_contours, -1, (0, 255, 0), 2)
-cv2.circle(contour_image_original, center, 5, (0, 0, 255), -1)  # Draw red circle based on input
+cv2.circle(contour_image_original, orig_center, 8, (255, 0, 0), -1)  # Draw red circle based on input
+
+
+##############################
+## Apply Concentric Circles ##
+##############################
+radii = 50
+num_of_circs = 22
+
+fit_poly = True
+boundary_ring = True
+interior_ring = False
+scale = 3/5
+rtol=1e-2
+atol=1e-6
+poly_deg = 2
+x_less = 600
+x_plus = 50
+blue_color = (255,0,0)
+red_color = (0,0,255)
+
+# Instatiate numpy array to store centers
+points = np.zeros(shape=(num_of_circs+1,2))
+points[0] = orig_center
+
+# Plot first point on path
+_, center = find_max_on_boundary(image_gray, orig_center,radii, rtol=rtol,atol=atol)
+points[1]=center
+# Draw rings
+if boundary_ring == True:
+    cv2.circle(contour_image_original, orig_center, radii, (0,0,255),1, lineType = cv2.LINE_AA)
+
+for step in range(2, num_of_circs+1):
+    radius = radii*step
+
+    # Draw interior ring
+    if interior_ring == True:
+        cv2.circle(contour_image_original,
+                   center,
+                   int(radius*scale),
+                   blue_color,
+                   1,
+                   lineType=cv2.LINE_AA)
+        
+    
+    # Get center of next point
+    ## Throw in try catch error --> break out of loop if does not work.
+    error_occured = False
+    try:
+        _, center = find_next_center(array=image_gray,
+                                    orig_center=orig_center,
+                                    neig_center=center,
+                                    r=radius,
+                                    scale=scale,
+                                    rtol=rtol,
+                                    atol=atol)
+    except Exception as e:
+        print("empty search")
+        error_occured = True
+    
+    if error_occured == True:
+        break   
+    
+    points[step] = center
+
+    # Draw next point
+    cv2.circle(contour_image_original, center,5,red_color,-1)
+
+    # Draw boundary ring
+    if boundary_ring == True:
+        cv2.circle(contour_image_original,
+                   center=orig_center,
+                   radius=radius,
+                   color=red_color,
+                   thickness=1,
+                   lineType=cv2.LINE_AA)
+
+
+####################
+## Apply Poly fit ##
+####################
+poly_coef = np.polyfit(points[:,0], points[:,1],deg=poly_deg)
+
+x = np.linspace(np.min(points[:,0])-x_less,np.max(points[:,0])+x_plus,100)
+y = poly_coef[0]*x**2 + poly_coef[1]*x+poly_coef[2]
+
+curve_img = np.zeros_like(contour_image_original)
+curve_points = np.column_stack((x,y)).astype(np.int32)
+
+cv2.polylines(curve_img, [curve_points], isClosed=False,color=blue_color,thickness=5)
+if fit_poly==True:
+    contour_image_original = cv2.addWeighted(contour_image_original,1,curve_img,1,0)
 
 #################################
 ## Apply smoothing to Contours ##
@@ -68,7 +158,7 @@ cv2.circle(contour_image_original, center, 5, (0, 0, 255), -1)  # Draw red circl
 smoothed_contours = []
 
 # Apply contour smoothing (contour approximation) to each selected contour
-epsilon = 125  # Adjust epsilon as needed
+epsilon = 100  # Adjust epsilon as needed
 for contour in selected_contours:
     smoothed_contours.append(cv2.approxPolyDP(contour, epsilon, True))
 
@@ -76,7 +166,7 @@ for contour in selected_contours:
 # Draw smoothed contours on the original image (or a copy of it)
 contour_image_smoothed = image.copy()
 cv2.drawContours(contour_image_smoothed, smoothed_contours, -1, (0, 255, 0), 2)
-cv2.circle(contour_image_smoothed, center, 5, (0, 0, 255), -1)  # Draw red circle based on input
+cv2.circle(contour_image_smoothed, orig_center, 5, (255, 0, 0), -1)  # Draw red circle based on input
 
 # Display the images side by side
 comparison_image = cv2.hconcat([contour_image_original, contour_image_smoothed])
