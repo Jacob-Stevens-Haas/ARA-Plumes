f70d427fc92b26c2d503f0f8fdd22d320ec4cd35
Jake <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2023-11-06 21:23:17 +0000

    CLN: Remove non-package files

--- test_functions/get_center.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('/Users/Malachite/Documents/UW/ARA/ARA-Plumes/')
-from utils import VideoPointPicker
-
-video_path = input("Please input image path:")
-
-vid_to_click = VideoPointPicker(video_path)
-vid_to_click.ask_user()
-print(vid_to_click.clicked_point)
