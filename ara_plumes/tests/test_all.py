d3c21115ce047080ba3d5882a8ab0e87f3dddb89
Ike <malachite.wind@gmail.com>
2024-02-25 14:35:05 -0800

    TESTING: added test for regression functions

--- ara_plumes/tests/test_all.py
+++ ara_plumes/tests/test_all.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from ara_plumes import models  # noqa: F401
-from ara_plumes import utils  # noqa: F401
+from ara_plumes import regressions
 
 
 def test_circle_poly_intersection_real():
@@ -20,3 +20,37 @@ def test_circle_poly_intersection_complex():
     expected = np.array([[-x0, y0], [-x1, y1], [x1, y1], [x0, y0]])
     result = models.PLUME.circle_poly_intersection(1, 0, 0, 1, 0, 0, False)
     np.testing.assert_array_almost_equal(result, expected)
+
+
+def test_regression_sinusoid():
+    regression_func = regressions.regression
+
+    # Generate data
+    A, w, g, B = (-0.1, 10, 4.14159265, 1)
+
+    def sinusoid_func(t, x):
+        return A * np.sin(w * x - g * t) + B * x
+
+    x = np.linspace(0, 1, 101)
+    t = np.array([1 for _ in range(len(x))])
+    Y = sinusoid_func(t, x)
+    X = np.hstack((t.reshape(-1, 1), x.reshape(-1, 1)))
+
+    expected = (A, w, g, B)
+    result = regression_func(X, Y, regression_method="sinusoid")
+
+    np.testing.assert_almost_equal(expected, result)
+
+
+def test_regression_poly():
+    regression_func = regressions.regression
+
+    def poly_func(x):
+        return x**2
+
+    x = np.linspace(0, 1, 101)
+    y = poly_func(x)
+
+    expected = (1, 0, 0)
+    result = regression_func(X=x, Y=y, regression_method="poly")
+    np.testing.assert_almost_equal(expected, result)
