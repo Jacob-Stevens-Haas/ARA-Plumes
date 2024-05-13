9b31da0911785deeb41ae75f86687796e02117cc
Ike Griss Salas <54322188+MalachiteWind@users.noreply.github.com>
2024-05-13 14:41:18 -0600

    Update ara_plumes/models.py
    Co-authored-by: Jacob Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>

--- ara_plumes/models.py
+++ ara_plumes/models.py
@@ -893,10 +893,10 @@ class PLUME:
             degree of polynomial used for regression (either explicit or parametric)
 
         x_less: int (default 600)
-            buffer on linspace for plotting on original image
+            Amount of extra pixels to the left of data to extrapolate fitted trajectories
 
         x_plus: int (default 0)
-            buffer on linspace for plotting on original image
+            Amount of extra pixels to the right of data to extrapolate fitted trajectories
 
         BGR_color: tuple (default (0,0,255))
             color of polynomial lines plotted on image
